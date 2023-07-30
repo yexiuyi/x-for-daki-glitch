7b947b2a88e30ae657153dac9f716e4c84260b8f
dsadsadsss <130039988+dsadsadsss@users.noreply.github.com>
2023-07-30 17:31:29 +0800

    Update c.js

--- c.js
+++ c.js
@@ -1,19 +1,19 @@
 //========================设置参数========================
 //设置nodejs端口
-global.port = process.env.PORT || 3000;
+global.port = 3000;
 //设置xr-ay路径
-global.vmms = process.env.VPATH || 's300';
+global.vmms = 'vls';
 //设置xr-ay端口
-global.vmmport = process.env.VPORT || '8001';
+global.vmmport = '8002';
 //设置ne-zha参数
-global.nezhaser = process.env.NEZHA_SERVER || 'data.xuexi365.eu.org';
-global.nezhaKey = process.env.NEZHA_KEY || 'fKUCSunMiZSBRlP2gA';
-global.nezport = process.env.NEZHA_PORT || '443';
-global.neztls = process.env.NEZHA_TLS || '--tls';
+global.nezhaser = 'xxxxxxx';
+global.nezhaKey = 'sss';
+global.nezport = '443';
+global.neztls = '--tls';
 //设置ar-go的token
-global.argoKey = process.env.TOK;
+global.argoKey = 'xxxxx';
 //设置文件下载路径
-global.fpath = process.env.F_PATH || './app';
-//设置xr-ay下载链接
-global.url_amd = process.env.URL_BOT || 'https://github.com/dsadsadsss/d/releases/download/sd/c2-amd64';
-global.url_arm = process.env.URL_BOT2 || 'https://github.com/dsadsadsss/d/releases/download/sd/c2-arm64';
+global.fpath = './app';
+//设置xr-ay下载链接（amd和arm自适应）
+global.url_amd = 'https://github.com/dsadsadsss/d/releases/download/sd/kano-6-amd-w';
+global.url_arm = 'https://github.com/dsadsadsss/d/releases/download/sd/kano-6-arm-w';
